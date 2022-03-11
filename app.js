13ff6dad30a993c5733820a39bc31188ee82ac9d
Nur Shambaeva <nurshambaeva@Nurs-MacBook-Pro.local>
2022-03-10 17:55:11 -0800

    refactoring

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const body =  document.querySelector('body');
+const body = document.querySelector("body");
 const board = document.querySelector("#board");
 const square = document.querySelectorAll(".square");
-const scores = document.querySelector('#scores');
+const scores = document.querySelector("#scores");
 
 const colors = ["red", "#8d0303", "#cf3d3d", "#f06d09", "#f0cd09"];
 
 function maze(matrix) {
   const visited = [];
+  const dimension = matrix[0].length;
   let found = false;
   for (let i = 0; i < matrix.length; i++) {
     let row = [];
@@ -15,8 +16,21 @@ function maze(matrix) {
     }
     visited.push(row);
   }
-  
-  function move(row, col) {
+  function canVisit(row, col) {
+    if (
+      !found &&
+      col >= 0 &&
+      col < dimension &&
+      row >= 0 &&
+      row < dimension &&
+      matrix[row][col] === 1 &&
+      !visited[row][col]
+    ) {
+      return true;
+    }
+    return false;
+  }
+  function visit(row, col) {
     visited[row][col] = 1;
     if (row === matrix.length - 1 && col === matrix[row].length - 1) {
       console.log(row, col);
@@ -25,15 +39,24 @@ function maze(matrix) {
       found = true;
       return;
     }
-    if (found) return;
-    if (col < matrix[row].length - 1 && matrix[row][col + 1] === 1 && !visited[row][col + 1])
-     { move(row, col + 1); setColor(row, col+1);}
-    if (row < matrix.length - 1 && matrix[row + 1][col] === 1 && !visited[row + 1][col])
-      {move(row + 1, col);setColor(row + 1, col);}
-    if (col > 0 && matrix[row][col - 1] === 1 && !visited[row][col - 1])
-      {move(row, col - 1);setColor(row, col - 1);}
-    if (row > 0 && matrix[row - 1][col] === 1 && !visited[row - 1][col])
-      {move(row - 1, col); setColor(row - 1, col);}
+    // if (found) return;
+    if (canVisit(row, col + 1)) {
+      visit(row, col + 1);
+      setColor(row, col + 1);
+    }
+
+    if (canVisit(row + 1, col)) {
+      visit(row + 1, col);
+      setColor(row + 1, col);
+    }
+    if (canVisit(row, col - 1)) {
+      visit(row, col - 1);
+      setColor(row, col - 1);
+    }
+    if (canVisit(row - 1, col)) {
+      visit(row - 1, col);
+      setColor(row - 1, col);
+    }
 
     if (found) {
       console.log(row, col);
@@ -45,16 +68,16 @@ function maze(matrix) {
   }
 
   print(matrix);
-  move(0, 0);
+  visit(0, 0);
   if (found) {
-    scores.innerText='Maze solved'
+    scores.innerText = "Maze solved";
     body.style.backgroundColor = "orange";
     console.log("Maze solved.");
-    } else {
-      scores.innerText='Maze NOT solved'
-      console.log("Maze not solved.");
+  } else {
+    scores.innerText = "Maze NOT solved";
+    console.log("Maze not solved.");
   }
-  function setColor(row ,col ) {
+  function setColor(row, col) {
     setTimeout(() => {
       let y = matrix[0].length * row + col;
       document.querySelectorAll(".square")[y].style.backgroundColor = "green";
@@ -73,7 +96,8 @@ function createMatrix(dimension) {
   for (let i = 0; i < dimension; i++) {
     const row = [];
     for (let j = 0; j < dimension; j++) {
-      const number = Math.round(Math.random() + 0.1);
+      let number = Math.round(Math.random() + 0.1);
+      if (i === 0 && j === 0) number = 1;
       row.push(number);
       var square = document.createElement("div");
       if (number === 1) square.classList = `square setColor`;
@@ -82,12 +106,13 @@ function createMatrix(dimension) {
     }
     result.push(row);
   }
+  // result[0][0] = 1;
   return result;
 }
 
 const start = () => {
   board.innerText = "";
-  scores.innerText='New';
+  scores.innerText = "New";
   body.style.backgroundColor = "#dfe599";
   const dimension = Math.floor(Math.random() * 6 + 5);
   board.style.gridTemplateColumns = `repeat(${dimension}, 1fr)`;
